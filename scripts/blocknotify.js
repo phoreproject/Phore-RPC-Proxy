686db7750013a386da9282f66b5c97cc84a09d49
anchaj <anchaj@users.noreply.github.com>
2018-06-12 20:11:01 +0200

    script for phored which can be used for notifying redis about new block / transaction hash

---
+++ scripts/blocknotify.js
@@ -0,0 +1,33 @@
+let redis = require('redis');
+let config = require('../config.js');
+
+if(process.argv.length < 4) {
+	console.log('No enough parameters');
+	process.exit(1);
+}
+
+if (process.argv[2] !== config.redis_blocknotify_key_name &&
+    process.argv[2] !== config.redis_mempoolnotify_key_name) {
+    console.log('Unsupported option used');
+    process.exit(1);
+}
+
+let client = redis.createClient(config.redis_port, config.redis_host);
+
+client.on('error', function(err){
+  console.log('Something went wrong ', err);
+  client.quit();
+  process.exit(2);
+});
+
+client.publish(process.argv[2], process.argv[3], (err, reply) => {
+	if (err !== null) {
+		console.log('Set error:', err);
+		client.quit();
+		process.exit(3);
+	}
+
+	console.log('Successfully publish', process.argv[2], '=', process.argv[3]);
+	client.quit();
+	process.exit(0);
+});
