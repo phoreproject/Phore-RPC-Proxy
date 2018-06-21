7ec09c124d0a778b53e4243a2d5ce0dfe2cb2a17
anchaj <anchaj@users.noreply.github.com>
2018-06-21 13:54:43 +0200

    add docker file for web service

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:10
+
+WORKDIR /usr/src/app/
+COPY package*.json /usr/src/app/
+RUN npm install
+COPY . /usr/src/app/
+
+ENV REDIS_HOST 127.0.0.1
+ENV REDIS_PORT 6379
+# phored host must be specified with protocol
+ENV PHORED_HOST http://127.0.0.1
+ENV PHORED_PORT 11772
+ENV RPC_USER phorerpc
+ENV RPC_PASS CLQAWNfstzFzq3xm1qpG4aX75U2CoVpZqBkkz4QvzY7b
+
+EXPOSE 80
+
+CMD npm start
--- config.js
+++ config.js
@@ -5,10 +5,6 @@ module.exports = {
     web_port: process.env.WEB_PORT != null ? process.env.WEB_PORT : 8080,
     phored_host: process.env.PHORED_HOST != null ? process.env.PHORED_HOST : 'http://127.0.0.1',
     phored_port: process.env.PHORED_PORT != null ? process.env.PHORED_PORT : 11772,
-    phored_exec: process.env.PHORED_EXEC != null ? process.env.PHORED_EXEC : './phored',
-    phore_cli: process.env.PHORE_CLI != null ? process.env.PHORE_CLI : './phore-cli',
     rpc_user: process.env.RPC_USER != null ? process.env.RPC_USER : 'phorerpc',
     rpc_pass: process.env.RPC_PASS != null ? process.env.RPC_PASS : 'CLQAWNfstzFzq3xm1qpG4aX75U2CoVpZqBkkz4QvzY7b',
-    backup_data_dir: process.env.PHORED_BACKUP_DIR != null ? process.env.PHORED_BACKUP_DIR : './backup',
-    phored_data_dir: process.env.PHORED_DATA_DIR != null ? process.env.PHORED_DATA_DIR : '../_data',
 };
