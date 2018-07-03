32d3a77253d1a1a6720c225e3f8f133628ee1b9a
anchaj <anchaj@users.noreply.github.com>
2018-07-03 23:32:17 +0200

    config S3 file must be string not bool

--- scripts/phored/config.js
+++ scripts/phored/config.js
@@ -3,7 +3,7 @@ module.exports = {
     redis_port: process.env.REDIS_PORT != null ? process.env.REDIS_PORT : 6379,
     backup_S3_dir: process.env.PHORED_BACKUP_S3_DIR != null ? process.env.PHORED_BACKUP_S3_DIR : 'phored-db-backup',
     backup_S3_region: process.env.PHORED_BACKUP_S3_REGION != null ? process.env.PHORED_BACKUP_S3_REGION : 'us-east-1',
-    backup_config_S3_file: process.env.PHORED_BACKUP_S3_INFO != null ? process.env.PHORED_BACKUP_S3_INFO != null : 'newest_prefix',
+    backup_config_S3_file: process.env.PHORED_BACKUP_S3_INFO != null ? process.env.PHORED_BACKUP_S3_INFO : 'newest_prefix',
     phored_data_dir: process.env.PHORED_DATA_DIR != null ? process.env.PHORED_DATA_DIR : '/root/.phore',
     start_from_beginning: process.env.START_FROM_BEGINNING != null ? process.env.START_FROM_BEGINNING : false,
 };
