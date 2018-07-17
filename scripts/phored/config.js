2f96b13cc4156a69fcf736d4affdb90c52785b1a
anchaj <anchaj@users.noreply.github.com>
2018-07-17 11:47:28 +0200

    add healthCheck.js which can check health of phored instance

--- scripts/phored/config.js
+++ scripts/phored/config.js
@@ -6,4 +6,8 @@ module.exports = {
     backup_config_S3_file: process.env.PHORED_BACKUP_S3_INFO != null ? process.env.PHORED_BACKUP_S3_INFO : 'newest_prefix',
     phored_data_dir: process.env.PHORED_DATA_DIR != null ? process.env.PHORED_DATA_DIR : '/root/.phore',
     start_from_beginning: process.env.START_FROM_BEGINNING != null ? process.env.START_FROM_BEGINNING : false,
+    rpc_user: process.env.RPC_USER != null ? process.env.RPC_USER : 'phorerpc',
+    rpc_pass: process.env.RPC_PASS != null ? process.env.RPC_PASS : 'CLQAWNfstzFzq3xm1qpG4aX75U2CoVpZqBkkz4QvzY7b',
+    phored_host: process.env.PHORED_HOST != null ? process.env.PHORED_HOST : 'http://127.0.0.1',
+    phored_port: process.env.PHORED_PORT != null ? process.env.PHORED_PORT : 11771,
 };
